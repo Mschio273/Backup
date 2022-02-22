fa2cb828b03c073eb314a67e01eab69bb4838267
Matheus Schio <matheus.schiod@gmail.com>
2022-02-22 15:29:20 -0300

    subindo swagger

---
+++ MockSwagger/src/test/java/com/example/mockswagger/MockSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
